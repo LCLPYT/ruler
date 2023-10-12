0e13ff10cf509dec2ced4524ff017dfccf82ef13
LCLP <lukas@lclpnet.work>
2023-10-12 18:52:40 +0200

    add mod setup

--- src/client/java/work/lclpnet/ruler/RulerClient.java
+++ src/client/java/work/lclpnet/ruler/RulerClient.java
@@ -1,8 +1,9 @@
-package com.example;
+package work.lclpnet.ruler;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class RulerClient implements ClientModInitializer {
+
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
--- src/main/java/work/lclpnet/ruler/ExampleMod.java
+++ src/main/java/work/lclpnet/ruler/ExampleMod.java
@@ -1,14 +1,13 @@
-package com.example;
+package work.lclpnet.ruler;
 
 import net.fabricmc.api.ModInitializer;
-
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExampleMod implements ModInitializer {
 
-	public static final String MOD_ID = "testmod";
+	public static final String MOD_ID = "ruler";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
